38027b17ea7687e882d43e54b1e8179a25a08128
Zoltanborbenyi <borbenyiz222@gmail.com>
2025-04-24 11:09:35 +0200

    A teszteket nem lehet valamiért rendesen futtatni, de megcsináltam a validációt és mentettem is ezeket.

--- UnitTestExample1/AccountControllerTestFixture.cs
+++ UnitTestExample1/AccountControllerTestFixture.cs
@@ -8,14 +8,13 @@ using System.Threading.Tasks;
 
 namespace UnitTestExample.Test
 {
-    [TestFixture] 
+    [TestFixture]
     public class AccountControllerTestFixture
     {
         [TestCase("abcd1234", false)]
         [TestCase("irf@uni-corvinus", false)]
         [TestCase("irf.uni-corvinus.hu", false)]
         [TestCase("irf@uni-corvinus.hu", true)]
-        [Test]
         public void TestValidateEmail(string email, bool expectedResult)
         {
             // Arrange
@@ -26,7 +25,6 @@ namespace UnitTestExample.Test
 
             // Assert
             Assert.That(actualResult, Is.EqualTo(expectedResult));
-
         }
 
         [TestCase("irf@uni-corvinus.hu", "Abcd1234")]
@@ -40,11 +38,10 @@ namespace UnitTestExample.Test
             var actualResult = accountController.Register(email, password);
 
             // Assert
-            // Assert
             Assert.That(actualResult.Email, Is.EqualTo(email));
             Assert.That(actualResult.Password, Is.EqualTo(password));
             Assert.That(actualResult.ID, Is.Not.EqualTo(Guid.Empty));
-
         }
     }
+
 }
