f13dcde87182bbc75eed9f440704ebfe1a46c81b
Zoltanborbenyi <borbenyiz222@gmail.com>
2025-04-24 10:43:34 +0200

    új osztály kész

---
+++ UnitTestExample1/AccountControllerTestFixture.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using System.Threading.Tasks;
+
+
+namespace UnitTestExample.Test
+{
+    [TestFixture]
+    public class AccountControllerTestFixture
+    {
+        [TestCase("abcd1234", false)]
+        [TestCase("irf@uni-corvinus", false)]
+        [TestCase("irf.uni-corvinus.hu", false)]
+        [TestCase("irf@uni-corvinus.hu", true)]
+        [Test]
+        public void TestValidateEmail(string email, bool expectedResult)
+        {
+            // Arrange
+            var accountController = new Controllers.AccountController();
+
+            // Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.That(actualResult, Is.EqualTo(expectedResult));
+
+        }
+
+        [TestCase("irf@uni-corvinus.hu", "Abcd1234")]
+        [TestCase("irf@uni-corvinus.hu", "Abcd1234567")]
+        public void TestRegisterHappyPath(string email, string password)
+        {
+            // Arrange
+            var accountController = new Controllers.AccountController();
+
+            // Act
+            var actualResult = accountController.Register(email, password);
+
+            // Assert
+            // Assert
+            Assert.That(actualResult.Email, Is.EqualTo(email));
+            Assert.That(actualResult.Password, Is.EqualTo(password));
+            Assert.That(actualResult.ID, Is.Not.EqualTo(Guid.Empty));
+
+        }
+    }
+}
---
+++ UnitTestExample1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestExample1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestExample1")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("957af4cb-eb10-48bf-bb79-97e5bfb82a32")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
