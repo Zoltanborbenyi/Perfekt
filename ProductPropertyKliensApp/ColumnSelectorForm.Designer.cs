edd1376566426c329169043de5a43c6d52d0f3ab
malomka <gabicamalomka@gmail.com>
2025-05-02 16:37:25 +0200

    ColumnSelectorForm.cs színei, gombjai, betűtípusai done

--- ProductPropertyKliensApp/ColumnSelectorForm.Designer.cs
+++ ProductPropertyKliensApp/ColumnSelectorForm.Designer.cs
@@ -35,40 +35,52 @@
             // 
             // checkedListBox1
             // 
+            this.checkedListBox1.Font = new System.Drawing.Font("Arial", 10F);
             this.checkedListBox1.FormattingEnabled = true;
-            this.checkedListBox1.Location = new System.Drawing.Point(42, 32);
+            this.checkedListBox1.Location = new System.Drawing.Point(63, 49);
+            this.checkedListBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(289, 364);
+            this.checkedListBox1.Size = new System.Drawing.Size(432, 544);
             this.checkedListBox1.TabIndex = 0;
             // 
             // Ok
             // 
-            this.Ok.Location = new System.Drawing.Point(42, 416);
+            this.Ok.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(9)))), ((int)(((byte)(9)))));
+            this.Ok.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ok.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(248)))), ((int)(((byte)(237)))));
+            this.Ok.Location = new System.Drawing.Point(62, 623);
+            this.Ok.Margin = new System.Windows.Forms.Padding(0);
             this.Ok.Name = "Ok";
-            this.Ok.Size = new System.Drawing.Size(144, 37);
+            this.Ok.Size = new System.Drawing.Size(216, 57);
             this.Ok.TabIndex = 1;
             this.Ok.Text = "Mentés";
-            this.Ok.UseVisualStyleBackColor = true;
+            this.Ok.UseVisualStyleBackColor = false;
             this.Ok.Click += new System.EventHandler(this.Ok_Click);
             // 
             // Cancel
             // 
+            this.Cancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(9)))), ((int)(((byte)(9)))));
             this.Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.Cancel.Location = new System.Drawing.Point(196, 416);
+            this.Cancel.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Cancel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(248)))), ((int)(((byte)(237)))));
+            this.Cancel.Location = new System.Drawing.Point(293, 623);
+            this.Cancel.Margin = new System.Windows.Forms.Padding(0);
             this.Cancel.Name = "Cancel";
-            this.Cancel.Size = new System.Drawing.Size(135, 37);
+            this.Cancel.Size = new System.Drawing.Size(202, 57);
             this.Cancel.TabIndex = 2;
             this.Cancel.Text = "Mégse";
-            this.Cancel.UseVisualStyleBackColor = true;
+            this.Cancel.UseVisualStyleBackColor = false;
             // 
             // ColumnSelectorForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(374, 462);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(248)))), ((int)(((byte)(237)))));
+            this.ClientSize = new System.Drawing.Size(561, 711);
             this.Controls.Add(this.Cancel);
             this.Controls.Add(this.Ok);
             this.Controls.Add(this.checkedListBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "ColumnSelectorForm";
             this.Text = "ColumnSelectorForm";
             this.Load += new System.EventHandler(this.ColumnSelectorForm_Load);
