dbb31c53118a90f0e782f66987b3dcd93eea9ab8
malomka <gabicamalomka@gmail.com>
2025-05-02 16:40:50 +0200

    ColumnSelectorForm.cs reszponzív, min és max size beállítva

--- ProductPropertyKliensApp/ColumnSelectorForm.Designer.cs
+++ ProductPropertyKliensApp/ColumnSelectorForm.Designer.cs
@@ -35,23 +35,28 @@
             // 
             // checkedListBox1
             // 
+            this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.checkedListBox1.Font = new System.Drawing.Font("Arial", 10F);
             this.checkedListBox1.FormattingEnabled = true;
             this.checkedListBox1.Location = new System.Drawing.Point(63, 49);
             this.checkedListBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.checkedListBox1.Name = "checkedListBox1";
-            this.checkedListBox1.Size = new System.Drawing.Size(432, 544);
+            this.checkedListBox1.Size = new System.Drawing.Size(414, 274);
             this.checkedListBox1.TabIndex = 0;
             // 
             // Ok
             // 
+            this.Ok.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.Ok.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(9)))), ((int)(((byte)(9)))));
             this.Ok.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Ok.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(248)))), ((int)(((byte)(237)))));
-            this.Ok.Location = new System.Drawing.Point(62, 623);
+            this.Ok.Location = new System.Drawing.Point(62, 356);
             this.Ok.Margin = new System.Windows.Forms.Padding(0);
             this.Ok.Name = "Ok";
-            this.Ok.Size = new System.Drawing.Size(216, 57);
+            this.Ok.Size = new System.Drawing.Size(198, 57);
             this.Ok.TabIndex = 1;
             this.Ok.Text = "Mentés";
             this.Ok.UseVisualStyleBackColor = false;
@@ -59,11 +64,12 @@
             // 
             // Cancel
             // 
+            this.Cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.Cancel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(166)))), ((int)(((byte)(9)))), ((int)(((byte)(9)))));
             this.Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.Cancel.Font = new System.Drawing.Font("Arial", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Cancel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(248)))), ((int)(((byte)(237)))));
-            this.Cancel.Location = new System.Drawing.Point(293, 623);
+            this.Cancel.Location = new System.Drawing.Point(275, 356);
             this.Cancel.Margin = new System.Windows.Forms.Padding(0);
             this.Cancel.Name = "Cancel";
             this.Cancel.Size = new System.Drawing.Size(202, 57);
@@ -76,11 +82,13 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(248)))), ((int)(((byte)(237)))));
-            this.ClientSize = new System.Drawing.Size(561, 711);
+            this.ClientSize = new System.Drawing.Size(543, 444);
             this.Controls.Add(this.Cancel);
             this.Controls.Add(this.Ok);
             this.Controls.Add(this.checkedListBox1);
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximumSize = new System.Drawing.Size(565, 1000);
+            this.MinimumSize = new System.Drawing.Size(565, 500);
             this.Name = "ColumnSelectorForm";
             this.Text = "ColumnSelectorForm";
             this.Load += new System.EventHandler(this.ColumnSelectorForm_Load);
