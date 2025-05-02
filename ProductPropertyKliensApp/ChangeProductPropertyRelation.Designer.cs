b399cc22c48d4ff7e18fe0e5f54101ee7ea865d1
malomka <gabicamalomka@gmail.com>
2025-05-02 16:23:21 +0200

    ProductTypeUserControl.cs reszponzív, valamint ChangeProductPropertyRelation.cs betűtípusai egységesek, hátterszíne beállítva

--- ProductPropertyKliensApp/ChangeProductPropertyRelation.Designer.cs
+++ ProductPropertyKliensApp/ChangeProductPropertyRelation.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label1 = new System.Windows.Forms.Label();
             this.listBox1 = new System.Windows.Forms.ListBox();
@@ -49,60 +50,73 @@
             // 
             // dataGridView1
             // 
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 10F);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(123)))), ((int)(((byte)(157)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(248)))), ((int)(((byte)(237)))));
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(16, 47);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Location = new System.Drawing.Point(18, 59);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.Size = new System.Drawing.Size(399, 550);
+            this.dataGridView1.Size = new System.Drawing.Size(449, 688);
             this.dataGridView1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(16, 11);
+            this.label1.Font = new System.Drawing.Font("Arial", 10F);
+            this.label1.Location = new System.Drawing.Point(14, 34);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(133, 16);
+            this.label1.Size = new System.Drawing.Size(198, 23);
             this.label1.TabIndex = 1;
             this.label1.Text = "Kiválasztott termékek";
             // 
             // listBox1
             // 
             this.listBox1.DataSource = this.bindingSource1;
+            this.listBox1.Font = new System.Drawing.Font("Arial", 10F);
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(443, 47);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBox1.ItemHeight = 23;
+            this.listBox1.Location = new System.Drawing.Point(496, 99);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(245, 548);
+            this.listBox1.Size = new System.Drawing.Size(275, 648);
             this.listBox1.TabIndex = 2;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(728, 47);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Font = new System.Drawing.Font("Arial", 10F);
+            this.textBox1.Location = new System.Drawing.Point(819, 59);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(312, 69);
+            this.textBox1.Size = new System.Drawing.Size(350, 85);
             this.textBox1.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(819, 14);
+            this.label2.Font = new System.Drawing.Font("Arial", 10F);
+            this.label2.Location = new System.Drawing.Point(817, 34);
             this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(125, 16);
+            this.label2.Size = new System.Drawing.Size(178, 23);
             this.label2.TabIndex = 4;
             this.label2.Text = "Tulajdonság Értéke";
             // 
             // UpdateProductPropertyBox
             // 
-            this.UpdateProductPropertyBox.Location = new System.Drawing.Point(729, 128);
-            this.UpdateProductPropertyBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.UpdateProductPropertyBox.Location = new System.Drawing.Point(820, 160);
+            this.UpdateProductPropertyBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.UpdateProductPropertyBox.Name = "UpdateProductPropertyBox";
-            this.UpdateProductPropertyBox.Size = new System.Drawing.Size(156, 37);
+            this.UpdateProductPropertyBox.Size = new System.Drawing.Size(176, 46);
             this.UpdateProductPropertyBox.TabIndex = 5;
             this.UpdateProductPropertyBox.Text = "Mentés";
             this.UpdateProductPropertyBox.UseVisualStyleBackColor = true;
@@ -110,40 +124,43 @@
             // 
             // TypePropertySearchBox
             // 
-            this.TypePropertySearchBox.Location = new System.Drawing.Point(441, 14);
-            this.TypePropertySearchBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.TypePropertySearchBox.Font = new System.Drawing.Font("Arial", 10F);
+            this.TypePropertySearchBox.Location = new System.Drawing.Point(496, 59);
+            this.TypePropertySearchBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.TypePropertySearchBox.Name = "TypePropertySearchBox";
-            this.TypePropertySearchBox.Size = new System.Drawing.Size(245, 22);
+            this.TypePropertySearchBox.Size = new System.Drawing.Size(275, 30);
             this.TypePropertySearchBox.TabIndex = 6;
             this.TypePropertySearchBox.TextChanged += new System.EventHandler(this.TypePropertySearchBox_TextChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(809, 256);
+            this.label3.Font = new System.Drawing.Font("Arial", 10F);
+            this.label3.Location = new System.Drawing.Point(817, 319);
             this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(145, 16);
+            this.label3.Size = new System.Drawing.Size(208, 23);
             this.label3.TabIndex = 8;
             this.label3.Text = "Jelenleg alkalmazottak";
             // 
             // listBox2
             // 
+            this.listBox2.Font = new System.Drawing.Font("Arial", 10F);
             this.listBox2.FormattingEnabled = true;
-            this.listBox2.ItemHeight = 16;
-            this.listBox2.Location = new System.Drawing.Point(728, 276);
-            this.listBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listBox2.ItemHeight = 23;
+            this.listBox2.Location = new System.Drawing.Point(819, 345);
+            this.listBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(311, 260);
+            this.listBox2.Size = new System.Drawing.Size(349, 303);
             this.listBox2.TabIndex = 9;
             this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
             // 
             // DeleteButton
             // 
-            this.DeleteButton.Location = new System.Drawing.Point(885, 128);
-            this.DeleteButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.DeleteButton.Location = new System.Drawing.Point(996, 160);
+            this.DeleteButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.DeleteButton.Name = "DeleteButton";
-            this.DeleteButton.Size = new System.Drawing.Size(156, 37);
+            this.DeleteButton.Size = new System.Drawing.Size(176, 46);
             this.DeleteButton.TabIndex = 10;
             this.DeleteButton.Text = "Törlés";
             this.DeleteButton.UseVisualStyleBackColor = true;
@@ -152,20 +169,21 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(727, 542);
+            this.label4.Font = new System.Drawing.Font("Arial", 6F);
+            this.label4.Location = new System.Drawing.Point(818, 653);
             this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(297, 16);
+            this.label4.Size = new System.Drawing.Size(271, 15);
             this.label4.TabIndex = 11;
             this.label4.Text = "A termékek típusainak megegyzőnek kell lenniük";
             this.label4.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(808, 560);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(913, 701);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(156, 37);
+            this.button1.Size = new System.Drawing.Size(176, 46);
             this.button1.TabIndex = 12;
             this.button1.Text = "Mentés";
             this.button1.UseVisualStyleBackColor = true;
@@ -173,18 +191,20 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(728, 176);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Font = new System.Drawing.Font("Arial", 10F);
+            this.textBox2.Location = new System.Drawing.Point(819, 220);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(312, 69);
+            this.textBox2.Size = new System.Drawing.Size(350, 85);
             this.textBox2.TabIndex = 13;
             // 
             // ChangeProductPropertyRelation
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1067, 607);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(248)))), ((int)(((byte)(237)))));
+            this.ClientSize = new System.Drawing.Size(1200, 759);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
@@ -198,7 +218,7 @@
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "ChangeProductPropertyRelation";
             this.Text = "ChangeProductPropertyRelation";
             this.Load += new System.EventHandler(this.ChangeProductPropertyRelation_Load);
